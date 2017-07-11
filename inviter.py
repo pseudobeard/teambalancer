9460e855ab86c1225f9f521ba98761c30c4b32b8
Matthew Winfield <matthewtwinfield@aol.com>
2017-07-11 10:24:03 +0100

    Increased auto-invite speed. Added protection against invalid battletags (now clicks back after invite)

--- inviter.py
+++ inviter.py
@@ -5,36 +5,35 @@ class Inviter:
     ADD_BUTTON = (1660, 320)
     VIA_BATTLETAG_BUTTON = (1200, 300)
     INVITE_BUTTON = (1000, 870)
+    BACK_BUTTON = (865, 865)
 
     def __init__(self):
         return
 
     def invite_player(self, battletag):
         click(self.ADD_BUTTON[0], self.ADD_BUTTON[1])  # Click on "Invite Players"
-        time.sleep(0.35)
+        time.sleep(0.3)
         click(self.VIA_BATTLETAG_BUTTON[0], self.VIA_BATTLETAG_BUTTON[1]) # Click on "Via Battletag"
-        time.sleep(0.35)
+        time.sleep(0.3)
 
         for character in battletag:
             if(character.isupper()): # Press shift when letter is uppercase
                 pressKey(0x10)
 
-            time.sleep(0.01)
-
             if(not (getKeyCode(character) is None)):
                 typeKey(getKeyCode(character)) # Type each letter of the battletag
-
-            time.sleep(0.01)
 
             releaseKey(0x10) # Release shift
 
             time.sleep(0.01)
 
-        time.sleep(0.35)
+        time.sleep(0.1)
 
         click(self.INVITE_BUTTON[0], self.INVITE_BUTTON[1]) # Click on the "Invite" button
 
-        time.sleep(0.35)
+        click(self.BACK_BUTTON[0], self.BACK_BUTTON[1]) # Click "Back" button (in case battletag did not work)
+
+        time.sleep(0.3)
 
     def invite_players(self, player_list):
         for p in player_list:
