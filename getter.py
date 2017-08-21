759df8269772ad09f54b237250b4a24259057311
Matthew Winfield <matthewtwinfield@aol.com>
2017-08-21 20:03:47 +0100

    Seperated item name from streamelements into constant

--- getter.py
+++ getter.py
@@ -4,6 +4,8 @@ import requests
 jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OTNiOTEzNjQ5YzVlYTczMzA1ZDkzYWYiLCJ1c2VybmFtZSI6ImthcmtldyIsInRva2VuIjoiR2ZmMExGT21xYjJqeXBsd3lBMFMiLCJpYXQiOjE0OTc4NjE3NTQsImlzcyI6IlN0cmVhbUVsZW1lbnRzIn0.fzv9g4zsGVW2DbI0YKh5mQj4jT_s8EVoofhKhfRN04k"
 id = "593b913649c5ea73305d93af" # Find these on https://streamelements.com/dashboard/account/information
 
+ITEM_NAME = "Viewer Game Sunday Ticket"
+
 headers = {"authorization" : "Bearer " + jwtToken}
 
 baseurl = "https://api.streamelements.com/kappa/v1/store/"
@@ -28,7 +30,7 @@ class Getter:
             item = redemption.get("item") # Get item
             if item is not None:
                 itemName = item.get("name")
-                if itemName == "Viewer Game Sunday Ticket": # If it is a viewer ticket, add the battletag to the list
+                if itemName == ITEM_NAME: # If it is a viewer ticket, add the battletag to the list
                     inputs = redemption.get("input")
                     battletags.append(inputs[0])
 
